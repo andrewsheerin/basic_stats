1d8ad6de86b36818b08b677232f13cb2f6b25a6c
Andrew Sheerin <andrewsheerin@Andrews-MacBook-Pro-5.local>
2021-11-27 19:18:03 -0500

    some updates

---
+++ basic_stats.py/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Basic Stats Package for Python
+    Version 1.0 Nov 27, 2021
+    Author:  Andrew Sheerin
+    Email: andrew_sheerin@uri.edu
+    # 
+    # DISCLAIMER:
+    #    This software is provided "as is" without warranty of any kind. 
+    #=========================================================================      
+"""
+
+#import numpy as np
+from .basic_stats import *
+
+
+__all__ = ['basic_stats']
